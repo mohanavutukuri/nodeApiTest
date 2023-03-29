3b6dee8081153ffa1dc523535f1401dc456ca0f7
mohanavutukuri <mohana.vutukuri@accolitedigital.com>
2023-03-29 14:53:21 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const app = require('express')();
+const httpServer = require('http').createServer(app);
+var fs = require("fs");
+
+const port = process.env.PORT || 3000;
+
+app.get('/listUsers', function (req, res) {
+    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+       console.log( data );
+       res.end( data );
+    });
+ })
+
+ httpServer.listen(port, () => console.log(`listening on port ${port}`));
