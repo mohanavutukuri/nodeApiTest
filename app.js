638d9e0b256851dc167d29ebdb2d1f631ae65644
mohanavutukuri <mohana.vutukuri@accolitedigital.com>
2023-03-31 18:37:48 +0530

    updating users when removed

--- app.js
+++ app.js
@@ -8,9 +8,20 @@ const io = require('socket.io')(httpServer, {
 const port = process.env.PORT || 3000;
 users1 = new Map();
 
+app.get('/:id',function(req,res){
+  var id = req.params['id'] 
+  fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+    JSON.parse(data).forEach(element => {
+      if(element.id==id)
+      res.json(element)
+    });
+
+  });
+});
+
 app.get('/', function (req, res) {
     fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-       res.end( data );
+      res.end( data );
     });
  })
 
@@ -24,12 +35,20 @@ app.get('/', function (req, res) {
   
 
     socket.on('message', (data) => {
-        io.to(users1.get(data.toId.toString())).emit('message', {"fromId":data.fromId,"message": data.message});
+        io.to(users1.get(data.toId.toString())).emit('message', data);
         // users1.get(data.toId).emit('message', {"fromId":data.fromId,"message": data.message});
     });
   
     socket.on('disconnect', () => {
-      console.log('a user disconnected!');
+      for (let [key, value] of users1.entries()) {
+        if (value === socket.id){
+          console.log('user '+ key +' disconnected');
+          users1.delete(key);
+        }
+        
+      }
+
+      io.emit('connection', [...users1.keys()]);
     });
   });
 
